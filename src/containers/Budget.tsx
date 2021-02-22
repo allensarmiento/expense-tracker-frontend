d6ce7e265633604a0169fd3cd0e689b5b39d3e4c
allensarmiento <allensarmiento3@gmail.com>
2021-02-21 22:46:33 -0800

    Add top banner and banner section

--- src/containers/Budget.tsx
+++ src/containers/Budget.tsx
@@ -1,8 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactChild } from 'react';
+import styled from 'styled-components';
 import DateBar from '../components/DateBar';
+import TopBanner from '../components/TopBanner';
+
+const Banner = styled.div`
+  grid-column: full-start / full-end;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(30rem, mincontent));
+  grid-template-rows: 100%;
+  grid-auto-flow: column;
+  grid-auto-columns: minmax(30rem, min-content);
+  grid-gap: 2rem;
+  padding: 2rem;
+  font-size: 2rem;
+  background-color: var(--top-banner-bg-color);
+  overflow-x: auto;
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface TitleProps {
+  justifySelf?: string;
+}
+
+const Title = styled.h3<TitleProps>`
+  justify-self: ${props => props.justifySelf || 'default'}
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+
+const Categories = styled.div``;
+
+const Category = styled.div``;
 
 const Budget = () => {
   const [items, setItems] = useState([]);
+  const categories = {};
   
   useEffect(() => {
     // Fetch Data Here
@@ -10,7 +46,16 @@ const Budget = () => {
 
   return (
     <div>
-      <DateBar year={2021} month={3} />
+      <Banner>
+        <Section>
+          <Title>Spending Categories</Title>
+          <Categories>
+            {Object.keys(categories).map((category, key) => (
+              <Category key={key}></Category>
+            ))}
+          </Categories>
+        </Section>
+      </Banner>
     </div>
   );
 };
